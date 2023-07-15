e63e3ed046b4f0b426f79a41dc334dc140beb049
shahank42 <crazykavlogs@gmail.com>
2023-07-16 00:58:40 +0530

    feat: added live md rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	let rawMarkdown = "";
+</script>
+
 <main class="h-64 flex flex-grow w-full bg-zinc-800">
 	<div class="bg-zinc-600 w-0 flex-grow">
-		<textarea class="w-full h-full" />
+		<textarea class="w-full h-full" bind:value={rawMarkdown} />
 	</div>
-	<div class="bg-zinc-400 w-0 flex-grow">preview</div>
+	<div class="bg-zinc-400 w-0 flex-grow">
+		{@html marked.parse(rawMarkdown, { mangle: false, headerIds: false })}
+	</div>
 </main>
