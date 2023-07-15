6d3f42a8c3e53619f50b4dbd635426463d51220f
shahank42 <crazykavlogs@gmail.com>
2023-07-16 03:08:27 +0530

    ui: removed a styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
 				placeholder="Start typing now..."
 				class="bg-zinc-100 text-md w-0 flex-grow p-3 font-mono resize-none focus:outline-none appearance-none"
 			/>
-			<article class={`w-0 flex-grow prose p-5 font-serif prose-xl prose-headings:mb-0 max-w-none max-h-[calc(100dvh-${titleBarHeight}px)] overflow-y-scroll`}>
+			<article class={`w-0 flex-grow prose p-5 font-serif prose-xl prose-headings max-w-none max-h-[calc(100dvh-${titleBarHeight}px)] overflow-y-scroll`}>
 				{@html marked.parse(rawMarkdown, { mangle: false, headerIds: false })}
 			</article>
 		</div>
