8ff6daa8af81d079c125623d808ae33c4c074642
shahank42 <crazykavlogs@gmail.com>
2023-07-15 22:23:09 +0530

    feat: added a layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="h-64 flex flex-grow w-full bg-zinc-800">
+	<div class="bg-zinc-600 w-0 flex-grow">
+		<textarea class="w-full h-full" />
+	</div>
+	<div class="bg-zinc-400 w-0 flex-grow">preview</div>
+</main>
