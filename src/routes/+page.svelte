bc7c93f064d7a91b6717ec4f9b0a0bb276b43275
shahank42 <crazykavlogs@gmail.com>
2023-07-16 02:56:39 +0530

    ui: modified a bunch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
 	import { marked } from 'marked';
 
-	let rawMarkdown = "";
+	let rawMarkdown = '';
+
+	let titleBarHeight = 0;
 </script>
 
-<main class="h-[99dvh] flex w-full">
-	<div class="w-0 flex-grow">
-		<textarea class="bg-zinc-100 w-full h-full p-3 font-mono resize-none focus:outline-none appearance-none" bind:value={rawMarkdown} />
-	</div>
-	<div class="w-0 flex-grow prose max-w-none p-3 overflow-y-scroll">
-		{@html marked.parse(rawMarkdown, { mangle: false, headerIds: false })}
+<main class="min-h-screen flex w-full">
+	<div class="bg-zinc-500 w-64">Files</div>
+	<div class="flex flex-col h-auto w-full">
+		<div class="bg-zinc-400" bind:clientHeight={titleBarHeight}>
+			<p class="text-3xl p-3">Title</p>
+		</div>
+		<div class="flex h-full w-full">
+			<textarea
+				bind:value={rawMarkdown}
+				placeholder="Start typing now..."
+				class="bg-zinc-100 text-md w-0 flex-grow p-3 font-mono resize-none focus:outline-none appearance-none"
+			/>
+			<article class={`w-0 flex-grow prose p-5 font-serif prose-xl prose-headings:mb-0 max-w-none max-h-[calc(100dvh-${titleBarHeight}px)] overflow-y-scroll`}>
+				{@html marked.parse(rawMarkdown, { mangle: false, headerIds: false })}
+			</article>
+		</div>
 	</div>
 </main>
