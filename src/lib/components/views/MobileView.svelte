115288eb639d2a6f3c089c1cf815c8ba3a109de3
shahank42 <crazykavlogs@gmail.com>
2023-07-24 23:13:10 +0530

    chore: switched the views

--- src/lib/components/views/MobileView.svelte
+++ src/lib/components/views/MobileView.svelte
@@ -13,7 +13,7 @@
 	import { languages } from '@codemirror/language-data';
 
 	let mdEditorViewNode: HTMLElement;
-	let mobileToggleView = false;
+	let toggleEdit = false;
 
 	afterUpdate(() => {
 		mdEditorViewNode.scrollTo({ top: 0 });
@@ -25,19 +25,29 @@
 		<!-- <button class="bg-zinc-100 hover:focus:bg-zinc-300 w-14 flex justify-center align-middle">
 			<img alt="Files" src={fileIconUrl} class="w-5" />
 		</button> -->
-		<input class="w-full px-4 text-xl focus:outline-none appearance-none bg-zinc-100" type="text" value="" placeholder="Untitled Note" />
+		<input
+			class="w-full px-4 text-xl focus:outline-none appearance-none bg-zinc-100"
+			type="text"
+			value=""
+			placeholder="Untitled Note"
+		/>
 	</div>
 
 	<div
 		bind:this={mdEditorViewNode}
 		class={`h-[calc(100dvh)] w-full inline overflow-x-hidden whitespace-nowrap ${
-			!mobileToggleView && 'overflow-y-hidden'
+			toggleEdit && 'overflow-y-hidden'
 		}`}
 	>
-		<div
-			class={`h-full w-full inline-block transition-transform ${
-				mobileToggleView && '-translate-x-full'
-			}`}
+		<article
+			class={`h-full bg-white inline-block transition-transform align-top prose font-serif prose-xl prose-headings w-full whitespace-normal p-5 ${
+				toggleEdit && '-translate-x-full'
+			} `}
+		>
+			{@html marked.parse($rawMarkdown, { mangle: false, headerIds: false })}
+		</article><!-- DO NOT REMOVE THESE COMMENT LINES BECAUSE SPACE MATTERS!!!
+		--><div
+			class={`h-full w-full inline-block transition-transform ${toggleEdit && '-translate-x-full'}`}
 		>
 			<CodeMirror
 				bind:value={$rawMarkdown}
@@ -50,34 +60,20 @@
 					}
 				}}
 			/>
-
-			<!-- <textarea
-				bind:value={$rawMarkdown}
-				placeholder="Start typing now..."
-				class={`h-full bg-zinc-200 w-full text-md p-3 font-mono resize-none focus:outline-none appearance-none`}
-			/> -->
 		</div>
-		<!-- DO NOT REMOVE THESE COMMENT LINES BECAUSE SPACE MATTERS!!!
-		-->
-		<article
-			class={`h-full bg-white inline-block transition-transform align-top prose font-serif prose-xl prose-headings w-full whitespace-normal p-5 ${
-				mobileToggleView && '-translate-x-full'
-			} `}
-		>
-			{@html marked.parse($rawMarkdown, { mangle: false, headerIds: false })}
-		</article>
+		
 	</div>
 
 	<div class="w-full bg-zinc-300 flex justify-center items-center h-20 md:hidden">
 		<div class="flex align-middle items-center">
-			<img alt="Edit" src={editIconUrl} class="mr-6 w-6" />
+			<img alt="Edit" src={previewIconUrl} class="mr-6 w-6" />
 			<label class="relative inline-flex items-center cursor-pointer">
-				<input type="checkbox" bind:checked={mobileToggleView} class="sr-only peer" />
+				<input type="checkbox" bind:checked={toggleEdit} class="sr-only peer" />
 				<div
 					class="w-14 h-7 peer-focus:ring-4 rounded-full peer bg-zinc-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all border-zinc-600"
 				/>
 			</label>
-			<img alt="Preview" src={previewIconUrl} class="ml-6 w-6" />
+			<img alt="Preview" src={editIconUrl} class="ml-6 w-6" />
 		</div>
 	</div>
 </div>
