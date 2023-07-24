732f6894eaa4701b5bfb920e617081eacc3ee258
shahank42 <crazykavlogs@gmail.com>
2023-07-24 22:47:07 +0530

    feat: integrated codemirror in the mobile view

--- src/lib/components/views/MobileView.svelte
+++ src/lib/components/views/MobileView.svelte
@@ -5,29 +5,57 @@
 	import editIconUrl from '$lib/assets/editIcon.svg?url';
 	import previewIconUrl from '$lib/assets/previewIcon.svg?url';
 	import fileIconUrl from '$lib/assets/fileIcon.svg?url';
+	import { afterUpdate } from 'svelte';
 
+	import CodeMirror from 'svelte-codemirror-editor';
+
+	import { markdown } from '@codemirror/lang-markdown';
+	import { languages } from '@codemirror/language-data';
+
+	let mdEditorViewNode: HTMLElement;
 	let mobileToggleView = false;
+
+	afterUpdate(() => {
+		mdEditorViewNode.scrollTo({ top: 0 });
+	});
 </script>
 
 <div class="h-full w-full flex flex-col">
 	<div class=" w-full h-14 flex">
-		<button class="bg-white hover:focus:bg-zinc-300 w-14 flex justify-center align-middle">
+		<!-- <button class="bg-zinc-100 hover:focus:bg-zinc-300 w-14 flex justify-center align-middle">
 			<img alt="Files" src={fileIconUrl} class="w-5" />
-		</button>
-		<input class="w-full px-3 text-xl" type="text" value="" placeholder="Untitled Note" />
+		</button> -->
+		<input class="w-full px-4 text-xl focus:outline-none appearance-none bg-zinc-100" type="text" value="" placeholder="Untitled Note" />
 	</div>
 
-	<div class="h-full w-full inline overflow-x-hidden whitespace-nowrap">
+	<div
+		bind:this={mdEditorViewNode}
+		class={`h-[calc(100dvh)] w-full inline overflow-x-hidden whitespace-nowrap ${
+			!mobileToggleView && 'overflow-y-hidden'
+		}`}
+	>
 		<div
 			class={`h-full w-full inline-block transition-transform ${
 				mobileToggleView && '-translate-x-full'
 			}`}
 		>
-			<textarea
+			<CodeMirror
+				bind:value={$rawMarkdown}
+				class="h-full text-base overflow-y-scroll"
+				lang={markdown()}
+				lineWrapping={true}
+				styles={{
+					'&': {
+						height: '100%'
+					}
+				}}
+			/>
+
+			<!-- <textarea
 				bind:value={$rawMarkdown}
 				placeholder="Start typing now..."
-				class="h-full bg-zinc-200 w-full text-md p-3 font-mono resize-none focus:outline-none appearance-none"
-			/>
+				class={`h-full bg-zinc-200 w-full text-md p-3 font-mono resize-none focus:outline-none appearance-none`}
+			/> -->
 		</div>
 		<!-- DO NOT REMOVE THESE COMMENT LINES BECAUSE SPACE MATTERS!!!
 		-->
@@ -40,7 +68,7 @@
 		</article>
 	</div>
 
-	<div class="w-full flex justify-center items-center h-20 md:hidden">
+	<div class="w-full bg-zinc-300 flex justify-center items-center h-20 md:hidden">
 		<div class="flex align-middle items-center">
 			<img alt="Edit" src={editIconUrl} class="mr-6 w-6" />
 			<label class="relative inline-flex items-center cursor-pointer">
